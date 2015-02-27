14272f17d73fc3b21edb9c786f5762974c6b3a96
GCorbel <guirec.corbel@gmail.com>
2015-02-27 05:21:23 -0500

    Add the scenario to add a new user

--- spec/acceptances/manage_spec.rb
+++ spec/acceptances/manage_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
-feature "Manage users", type: :feature do
-  scenario "Show the list of users", js: true do
-    User.create!(name: "Guirec Corbel", age: 29)
+feature 'Manage users', type: :feature do
+  scenario 'Show the list of users', js: true do
+    User.create!(name: 'Guirec Corbel', age: 29)
     visit users_path
-    expect(page).to have_content("Guirec Corbel")
+    expect(page).to have_content('Guirec Corbel')
+  end
+
+  scenario 'Create a new user', js: true do
+    visit users_path
+    click_on 'Add a new'
+    fill_in 'Name', with: 'Guirec Corbel'
+    fill_in 'Age', with: 29
+    click_on 'Save changes'
+
+    expect(page).to have_content('Guirec Corbel')
   end
 end
