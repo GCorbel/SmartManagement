4987c6ed9bc98c12dfa7903ac5f7af2847141719
GCorbel <guirec.corbel@gmail.com>
2015-02-27 05:33:45 -0500

    Add a scenario to edit a user

--- spec/acceptances/manage_spec.rb
+++ spec/acceptances/manage_spec.rb
@@ -4,7 +4,10 @@ feature 'Manage users', type: :feature do
   scenario 'Show the list of users', js: true do
     User.create!(name: 'Guirec Corbel', age: 29)
     visit users_path
-    expect(page).to have_content('Guirec Corbel')
+
+    within('#users_table') do
+      expect(page).to have_content('Guirec Corbel')
+    end
   end
 
   scenario 'Create a new user', js: true do
@@ -14,6 +17,21 @@ feature 'Manage users', type: :feature do
     fill_in 'Age', with: 29
     click_on 'Save changes'
 
-    expect(page).to have_content('Guirec Corbel')
+    within('#users_table') do
+      expect(page).to have_content('Guirec Corbel')
+    end
+  end
+
+  scenario 'Update a user', js: true do
+    User.create!(name: 'Guirec Corbel', age: 29)
+
+    visit users_path
+    click_on 'Edit'
+    fill_in 'Name', with: 'Guirec Corbel2'
+    click_on 'Save changes'
+
+    within('#users_table') do
+      expect(page).to have_content('Guirec Corbel2')
+    end
   end
 end
